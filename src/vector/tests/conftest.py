530f7ac2438e8e3d82d9e471907ebced4cf7e924
alokaj <alba.lokaj@actian.com>
2026-01-27 10:17:45 -0600

    Add testing for sse transport

--- src/vector/tests/conftest.py
+++ src/vector/tests/conftest.py
@@ -6,34 +6,55 @@ from fastmcp import Client, FastMCP
 from actian_mcp_server.server import server_name, app_lifespan
 from types import SimpleNamespace
 from multiprocessing import Process
-import time
+import socket
 
 CONF_ARGS = {
     "driver": "{Ingres VW}",
     "database": "mcp_vector_db",
     "max_connections": 10,
     "host": "127.0.0.1",
-    "port": 55555
+    "port": 0,
 }
 
-@pytest.fixture()
-def server_url(server_localhost):
-    return f'http://{CONF_ARGS["host"]}:{CONF_ARGS["port"]}/mcp'
+def get_free_port():
+    with socket.socket() as s:
+        s.bind(('', 0))
+        port = s.getsockname()[1]
+    return port
 
-def _server_localhost():
-    transport_mode = "http"
+def _server_localhost(_transport, _port):
+    conf_args = dict(CONF_ARGS)
+    conf_args["port"] = _port
     server = FastMCP(
         server_name,
-        lifespan=app_lifespan(SimpleNamespace(dbms="vector", transport=transport_mode), conf_file_args=CONF_ARGS),
+        lifespan=app_lifespan(
+            SimpleNamespace(dbms="vector", transport=_transport),
+            conf_file_args=conf_args,
+        ),
     )
-    server.run(transport=transport_mode, host=CONF_ARGS["host"], port=CONF_ARGS["port"])
+    server.run(transport=_transport, host=conf_args["host"], port=conf_args["port"])
 
 @pytest.fixture()
-def server_localhost():
-    proc = Process(target=_server_localhost, daemon=True)
+def server_localhost_http():
+    transport = "http"
+    port = get_free_port()
+    proc = Process(target=_server_localhost, args=(transport, port,), daemon=True)
     proc.start()
     try:
-        yield
+        yield "http://%s:%s/mcp" % (CONF_ARGS["host"], port)
+    finally:
+        if proc.is_alive():
+            proc.terminate()
+            proc.join()
+
+@pytest.fixture()
+def server_localhost_sse():
+    transport = "sse"
+    port = get_free_port()
+    proc = Process(target=_server_localhost, args=(transport, port), daemon=True)
+    proc.start()
+    try:
+        yield "http://%s:%s/sse" % (CONF_ARGS["host"], port)
     finally:
         if proc.is_alive():
             proc.terminate()
@@ -41,10 +62,10 @@ def server_localhost():
 
 @pytest.fixture()
 def server_stdio():
-    transport_mode = "stdio"
+    transport = "stdio"
     return FastMCP(
         server_name,
-        lifespan=app_lifespan(SimpleNamespace(dbms="vector", transport=transport_mode), conf_file_args=CONF_ARGS),
+        lifespan=app_lifespan(SimpleNamespace(dbms="vector", transport=transport), conf_file_args=CONF_ARGS),
     )
 
 @pytest.fixture()
--- src/vector/tests/test_server_multitenancy.py
+++ src/vector/tests/test_server_multitenancy.py
@@ -5,15 +5,27 @@ from fastmcp import Client
 import asyncio
 import toons
 
-async def connect_http_clients(url, num):
+async def connect_http_clients(url):
     async with Client(url) as client:
         result = await client.call_tool("execute_query", {"query": "select total_amount from orders where order_id=52"})
         return result.content[0].text
 
-async def test_parallel_clients(server_url):
+async def test_parallel_clients_http(server_localhost_http):
     num_http_clients = 100
+    server_url = server_localhost_http
     expected_result = toons.dumps([{'total_amount': '45.99'}])
-    connections = [connect_http_clients(server_url, num) for num in range(num_http_clients)]
+    connections = [connect_http_clients(server_url) for _ in range(num_http_clients)]
+    results = await asyncio.gather(*connections)
+    
+    assert len(results) == num_http_clients
+    for result in results:
+        assert result == expected_result
+
+async def test_parallel_clients_sse(server_localhost_sse):
+    num_http_clients = 100
+    server_url = server_localhost_sse
+    expected_result = toons.dumps([{'total_amount': '45.99'}])
+    connections = [connect_http_clients(server_url) for _ in range(num_http_clients)]
     results = await asyncio.gather(*connections)
     
     assert len(results) == num_http_clients
