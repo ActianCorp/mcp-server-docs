453d759c10f8f9e8a3cd49eb9015080c2667bcd5
ninturi <nagaraju.inturi@actian.com>
2026-01-29 16:29:40 -0600

    delete unused file

--- src/actian_mcp_server/globals.py
+++
@@ -1,6 +0,0 @@
-from contextvars import ContextVar
-
-# Define a global context variable to store the extracted database username.
-# It is "Async Safe" - different requests won't overwrite each other.
-# Default value is None.
-current_username: ContextVar[str | None] = ContextVar("current_username", default=None)
