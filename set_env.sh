b0882c1e7206a301884a3ccff384560eab261aa7
alokaj <alba.lokaj@actian.com>
2026-02-16 11:44:28 -0600

    Fix pytests after latest changes

--- set_env.sh
+++ set_env.sh
@@ -1,3 +1,3 @@
-MCP_SERVER_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-CONTAINER_BASE_NAME_MCP_SERVER_LINUX=actian/vector-mcp-server-linux
-CONTAINER_VERSION_MCP_SERVER_LINUX=test
+export MCP_SERVER_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+export CONTAINER_BASE_NAME_MCP_SERVER_LINUX=actian/vector-mcp-server-linux
+export CONTAINER_VERSION_MCP_SERVER_LINUX=test
